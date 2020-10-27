f455e8e1fb8a4ba186cca9242ce6870cc8474bb3
ducduy <44523226+phamnguyenducduy@users.noreply.github.com>
2020-10-27 09:38:58 +0700

    up

---
+++ src/firebaseConfig.js
@@ -0,0 +1,15 @@
+export default {
+  apiKey: "AIzaSyBHwJAg-DWKq2O6Nx5cmkmLHgD15bY9its",
+  authDomain: "project-vip-eac82.firebaseapp.com",
+  databaseURL: "https://project-vip-eac82.firebaseio.com",
+  projectId: "project-vip-eac82",
+  storageBucket: "project-vip-eac82.appspot.com",
+  messagingSenderId: "489589473250",
+  appId: "1:489589473250:web:7af0770494867ca38f43bb"
+  // apiKey: "AIzaSyA3x2WuF_m9E4zKYhInHVj1fsqToe-klEQ",
+  // authDomain: "webapp-6bf36.firebaseapp.com",
+  // databaseURL: "https://webapp-6bf36.firebaseio.com",
+  // projectId: "webapp-6bf36",
+  // storageBucket: "webapp-6bf36.appspot.com",
+  // messagingSenderId: "781463977800"
+}
